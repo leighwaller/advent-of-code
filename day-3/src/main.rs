7b76e061e782a685e07651468152c7e25cc73199
leigh_waller <leigh.waller2@icloud.com>
2019-12-18 22:57:36 +1100

    Ensure all lines read and refactor

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,38 +1,70 @@
-use std::fs::{File};
-use std::io::Read;
+use std::fs::File;
+use std::io::{BufReader, BufRead, Lines};
 
 fn main() {
-    let data = read_input("data/input.csv");
-    for command in data {
-        println!("Processing: {}", command);
+    process(read_file("data/input.csv").lines())
+}
+
+fn process(lines: Lines<BufReader<File>>) {
+    for line in lines {
+        let data = parse_line(line.unwrap());
+        process_line(data)
     }
 }
 
-struct Command {
-    direction: Direction,
-    distance: u16
+fn process_line(data: Vec<String>) {
+    for input in data {
+        println!("Processing: {}", input);
+        let (direction, distance) = input.split_at(1);
+        let command = Command::new(direction, distance);
+        // todo
+    }
 }
 
+#[derive(Debug)]
+struct Command {
+    direction: Direction,
+    distance: u16,
+}
+
+#[derive(Debug)]
 enum Direction {
     Left,
     Right,
     Up,
-    Down
+    Down,
 }
 
-fn parse_direction(c: char) -> Direction {
-    return match c {
-        'L' => Direction::Left,
-        'R' => Direction::Right,
-        'U' => Direction::Up,
-        'D' => Direction::Down,
-        _ => panic!("Unknown direction code: {}", c)
+impl Command {
+    fn new(direction: &str, distance: &str) -> Command {
+        return Command {
+            direction: Direction::new(direction),
+            distance: distance.parse().unwrap(),
+        };
     }
 }
 
-fn read_input(path: &str) -> Vec<String> {
-    // assumes only one line of input
-    let mut line = String::new();
-    File::open(path).unwrap().read_to_string(&mut line);
-    return line.trim().split(",").map(str::to_string).collect();
+impl Direction {
+    fn new(direction: &str) -> Direction {
+        let c = direction.chars().nth(0).unwrap();
+        return match c {
+            'L' => Direction::Left,
+            'R' => Direction::Right,
+            'U' => Direction::Up,
+            'D' => Direction::Down,
+            _ => panic!("Unknown direction code: {}", c)
+        };
+    }
+}
+
+fn parse_line(line: String) -> Vec<String> {
+    return line.trim()
+        .split(",")
+        .map(str::to_string)
+        .collect();
+}
+
+fn read_file(path: &str) -> BufReader<File> {
+    let file = File::open(path).unwrap();
+    return BufReader::new(file);
 }
